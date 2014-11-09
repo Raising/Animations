359c7bc704faf2645b973becb3c4be2e081f5a60
Ignacio Medina Castillo <raising_spirit@hotmail.com>
2014-11-09 18:19:07 +0100

    animaciones de oscilacion 3d

--- 14_10_28_Bloque 1/AnimationRaisingModule.js
+++ 14_10_28_Bloque 1/AnimationRaisingModule.js
@@ -7,20 +7,26 @@ var ARM = {version:"1"};
 
 
 
-ARM.clipedImageColumToSide = function(imagePath,sizeH,sizeV,columWidth,direction){//
+ARM.clipedImageColumToSide = function(imagePath,duration,sizeH,sizeV,columWidth,direction){//
 
  //ease:Circ.easeInOut}
 
 	var divimage = $('<div>');
 	var image = $('<img>');//
 	var end =(columWidth+sizeH);
+	var doubletall = sizeV*4;
+	var exterminate = function(){
+		divimage.remove();
+		console.log("divimage exterminted");
+	} ;
+
 	var onloadHandler = function () {
 		
 		if (direction === "toLeft"){ 
-			TweenMax.to(divimage,12, {css:{clip:'rect(0px, 0px, '+sizeV+'px, -'+columWidth+'px)'}, ease:Elastic.easeOut});
+			TweenMax.to(divimage,duration, {css:{clip:'rect(-800px, 0px, '+doubletall+'px, -'+columWidth+'px)'}/*, ease:Elastic.easeOut*/, onComplete: exterminate});
 		}
 		else if (direction === "toRight"){
-			TweenMax.to(divimage, 12, {css:{clip:"rect(0px,"+end+"px, "+sizeV+"px,"+sizeH+"px)"}, ease:Elastic.easeOut});
+			TweenMax.to(divimage, duration, {css:{clip:"rect(-800px,"+end+"px, "+doubletall+"px,"+sizeH+"px)"}/*, ease:Elastic.easeOut*/, onComplete: exterminate });
 		}else{
 			console.log("argumento de direccion incorrecto");
 		}
@@ -31,22 +37,24 @@ ARM.clipedImageColumToSide = function(imagePath,sizeH,sizeV,columWidth,direction
 		width:"sizeH",
 		height:"sizeV",
 		left:"0px",
-		top:"0px"});
+		top:"0px",
+		transform: "rotate(30deg)"});
 		
 	
 	divimage.css({position:"absolute",
-		width:"sizeH",
-		height:"sizeV",
+		width:sizeH*2+"px",
+		height:sizeV*2+"px",
 		left:"0px",
-		top:"0px"});
+		top:"-400px",
+		transform: "rotate(-30deg)"});
 	
 	divimage.append(image);
 	$(document.body).append(divimage);
 
 	if (direction === "toLeft"){
-		divimage.css('clip', "rect(0px,"+end+"px, "+sizeV+"px,"+sizeH+"px)");
+		divimage.css('clip', "rect(-800px,"+end+"px, "+doubletall+"px,"+sizeH+"px)");
 	}else if (direction === "toRight"){
-		divimage.css('clip', 'rect(0px, 0px, '+sizeV+'px, -'+columWidth+'px)');
+		divimage.css('clip', 'rect(-800px, 0px, '+doubletall+'px, -'+columWidth+'px)');
 	}else {
 		console.log("argumento de direccion incorrecto");
 	}
@@ -56,4 +64,104 @@ ARM.clipedImageColumToSide = function(imagePath,sizeH,sizeV,columWidth,direction
 	image.attr('src', imagePath);
 	
 
+
 } 
+
+
+ARM.hangingImage = function(imagePath,targetImg,duration,sizeH,sizeV){
+
+
+	var image = $('#'+targetImg);//
+
+TweenLite.set(image, {transformPerspective:800});
+	var onloadHandler = function () {
+		
+		TweenMax.fromTo	(image, duration, {rotationX:-90, transformOrigin:"left top"},{rotationX:0, transformOrigin:"left top",ease:Elastic.easeOut });
+    }
+
+	image.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"100px",
+		top:"100px",
+		transform: "rotateY(10deg)"
+		
+		});
+		
+
+	
+	image.load(onloadHandler);
+	image.attr('src', imagePath);
+
+}
+
+ARM.tonelVerticalElastico = function(targetDiv,duration,sizeH,sizeV){
+
+	var animatedDiv = $('#'+targetDiv);
+TweenLite.set(animatedDiv, {transformPerspective:800});
+	var onloadHandler = function () {
+		
+		TweenMax.fromTo	(animatedDiv, duration, {rotationY:90, transformOrigin:"left 50% -400"},{rotationY:0, transformOrigin:"left 50% -200",ease:Elastic.easeOut });
+    }
+
+
+	animatedDiv.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"100px",
+		top:"100px"
+		});
+		
+	//animatedDiv.load(onloadHandler);
+	onloadHandler();
+}
+
+ARM.tonelVertical = function(targetDiv,duration,sizeH,sizeV){
+
+	var animatedDiv = $('#'+targetDiv);
+	TweenLite.set(animatedDiv, {transformPerspective:800});
+	var onloadHandler = function () {
+	
+		TweenMax.fromTo	(animatedDiv, duration, {rotationY:90, transformOrigin:"50% 50% 0"},{rotationY:0, transformOrigin:"50% 50% -1000",ease:Sine.easeInOut});
+    }
+
+
+	animatedDiv.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"100px",
+		top:"100px"
+		});
+		
+	//animatedDiv.load(onloadHandler);
+	onloadHandler();
+	
+}
+
+ARM.recursivetest = function(targetDiv,duration,sizeH,sizeV,number){
+if(number==0){}else{
+	var animatedDiv = $('#'+targetDiv);
+	TweenLite.set(animatedDiv, {transformPerspective:800});
+	var onloadHandler = function () {
+	
+		TweenMax.fromTo	(animatedDiv, duration, {rotationY:90, transformOrigin:"50% 50% 0"},{rotationY:0, transformOrigin:"50% 50% -1000",ease:Elastic.easeOut});
+    }
+
+	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+	animatedDiv.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"2%",
+		top:"2%",
+		backgroundColor:randomColor
+		});
+		
+	//animatedDiv.load(onloadHandler);
+	onloadHandler();
+
+	var next = $('<div id="'+targetDiv+'a"></div>');
+	animatedDiv.append(next);
+
+	ARM.recursivetest(targetDiv+'a',duration,sizeH*0.95,sizeV*0.95,number-1);
+	 }
+}
--- 14_10_28_Bloque 1/functionality.js
+++ 14_10_28_Bloque 1/functionality.js
@@ -3,68 +3,27 @@
 
 $(document).ready(function() {
 
+/*	
+	ARM.clipedImageColumToSide("img/landscape_training.jpg",13,640,400,100,"toRight");
+	ARM.clipedImageColumToSide("img/landscape_training.jpg",14,640,400,100,"toLeft");
+ 	ARM.clipedImageColumToSide("img/landscape_training.jpg",15,640,400,200,"toRight");
+*/
 	
-	ARM.clipedImageColumToSide("img/landscape_training.jpg",1280,1024,100,"toLeft");
+/*
+	ARM.tonelVerticalElastico("side3d",5,400,300);
+	ARM.hangingImage("img/landscape_training.jpg","test",5,200,200);
+*/
+
+/*
+	ARM.tonelVertical("side3d",5,400,300);
+	ARM.hangingImage("img/landscape_training.jpg","test",5,200,200);
+*/
+
+	ARM.recursivetest("side3d",5,400,300,50);
 
 });
 
- 
-
-/*
-var divImage1 = $('<div>');
-var onloadHandler = function () {
-TweenMax.to(image1, 3, {css:{clip:"rect(0px,275px,95px,0px)"}});
-// TweenMax.to(image1, 3, {css:{left: "500px"}});
-}
-
-var image1 = $('<img>');//
-image1.css('position', 'absolute');
-image1.css('left','200px');
-divImage1.css('top','0px');
-divImage1.css('position', 'absolute');
-divImage1.append(image1);
-$(document.body).append(divImage1);
-image1.css('clip', 'rect(30px, 80px, 80px, 30px)');
-image1.load(onloadHandler);
-image1.attr('src', 'https://www.google.com/images/srpr/logo3w.png');
-
-*/
 
 
 
 
-
-
-/*
-
-
-
-
-
-window.addEventListener("load", function(event) { // OJO quizas sea preferible on DOM ready
-    console.log("DOM fully loaded and parsed");
-
-    	 var logo = document.getElementById("logo");
-    	
-    	TweenLite.to(logo, 1, {left:"632px"});
-//It's important that you remove all dashes (-) from the css property names and use camelCase.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
-*/
