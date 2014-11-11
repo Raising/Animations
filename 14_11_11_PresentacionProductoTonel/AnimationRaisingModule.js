6c084ad8e9434869f2bb4ddca8fd661725c243e6
Ignacio Medina Castillo <raising_spirit@hotmail.com>
2014-11-11 16:56:54 +0100

    movimientos de divs ciclicos

---
+++ 14_11_11_PresentacionProductoTonel/AnimationRaisingModule.js
@@ -0,0 +1,194 @@
+/*
+ARM, Animation Raising Module.
+@autor: Ignacio Medina Castillo, AKA Raising.
+*/
+
+var ARM = {version:"1"};
+
+
+
+ARM.clipedImageColumToSide = function(imagePath,duration,sizeH,sizeV,columWidth,direction){//
+
+ //ease:Circ.easeInOut}
+
+	var divimage = $('<div>');
+	var image = $('<img>');//
+	var end =(columWidth+sizeH);
+	var doubletall = sizeV*4;
+	var exterminate = function(){
+		divimage.remove();
+		console.log("divimage exterminted");
+	} ;
+
+	var onloadHandler = function () {
+		
+		if (direction === "toLeft"){ 
+			TweenMax.to(divimage,duration, {css:{clip:'rect(-800px, 0px, '+doubletall+'px, -'+columWidth+'px)'}/*, ease:Elastic.easeOut*/, onComplete: exterminate});
+		}
+		else if (direction === "toRight"){
+			TweenMax.to(divimage, duration, {css:{clip:"rect(-800px,"+end+"px, "+doubletall+"px,"+sizeH+"px)"}/*, ease:Elastic.easeOut*/, onComplete: exterminate });
+		}else{
+			console.log("argumento de direccion incorrecto");
+		}
+    }
+
+
+	image.css({position:"absolute",
+		width:"sizeH",
+		height:"sizeV",
+		left:"0px",
+		top:"0px",
+		transform: "rotate(30deg)"});
+		
+	
+	divimage.css({position:"absolute",
+		width:sizeH*2+"px",
+		height:sizeV*2+"px",
+		left:"0px",
+		top:"-400px",
+		transform: "rotate(-30deg)"});
+	
+	divimage.append(image);
+	$(document.body).append(divimage);
+
+	if (direction === "toLeft"){
+		divimage.css('clip', "rect(-800px,"+end+"px, "+doubletall+"px,"+sizeH+"px)");
+	}else if (direction === "toRight"){
+		divimage.css('clip', 'rect(-800px, 0px, '+doubletall+'px, -'+columWidth+'px)');
+	}else {
+		console.log("argumento de direccion incorrecto");
+	}
+
+	
+	image.load(onloadHandler);
+	image.attr('src', imagePath);
+	
+
+
+} 
+
+
+ARM.hangingImage = function(imagePath,targetImg,duration,sizeH,sizeV,aditionalArgs){
+
+	aditionalArgs = aditionalArgs || "";
+	var image = $('#'+targetImg);
+
+	TweenLite.set(image, {transformPerspective:800});
+
+	image.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"100px",
+		top:"10px",
+		transform:"translate3d(0.5px,0,0)",
+		transform: "rotateY(50deg)"
+		
+		});
+		
+
+	
+	//image.load(onloadHandler);
+	image.attr('src', imagePath);
+	return('.from($("#'+targetImg+'"),'+duration+', {rotationX:-90,autoAlpha:0, transformOrigin:"50% top",yoyo:true,repeat: 1,ease:Elastic.easeOut }'+aditionalArgs+')');
+
+}
+
+ARM.tonelVerticalElastico = function(targetDiv,duration,sizeH,sizeV,aditionalArgs){
+	aditionalArgs = aditionalArgs || "";
+	var animatedDiv = $('#'+targetDiv);
+	TweenLite.set(animatedDiv, {transformPerspective:800});
+	/*var onloadHandler = function () {
+		
+		TweenMax.fromTo	(animatedDiv, duration, {rotationY:90, transformOrigin:"left 50% -400"},{rotationY:0, transformOrigin:"left 50% -200",ease:Elastic.easeOut });
+    }*/
+	animatedDiv.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"100px",
+		top:"100px"
+		});
+		
+	
+	//onloadHandler();
+	return('.fromTo($("#'+targetDiv+'"),'+duration+', {rotationY:90, transformOrigin:"left 50% -400"},{rotationY:0, transformOrigin:"left 50% -200",ease:Elastic.easeOut }'+aditionalArgs+')');
+}
+
+ARM.tonelVertical = function(targetDiv,duration,sizeH,sizeV,aditionalArgs){
+	aditionalArgs = aditionalArgs || "";
+	var animatedDiv = $('#'+targetDiv);
+	TweenLite.set(animatedDiv, {transformPerspective:800});
+	animatedDiv.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"100px",
+		top:"100px"
+		});
+	return('.fromTo($("#'+targetDiv+'"), '+duration+', {rotationY:90, transformOrigin:"50% 50% 0"},{rotationY:0, transformOrigin:"50% 50% -1000",ease:Sine.easeInOut}'+aditionalArgs+')');
+}
+
+
+
+
+
+
+
+ARM.recursivetest = function(targetDiv,duration,sizeH,sizeV,number){
+if(number==0){}else{
+	var animatedDiv = $('#'+targetDiv);
+	TweenLite.set(animatedDiv, {transformPerspective:800});
+	/*var onloadHandler = function () {
+	
+		TweenMax.fromTo	(animatedDiv, duration,{rotationY:90, transformOrigin:"50% 50% 0"},{rotationY:0, transformOrigin:"50% 50% -1000",ease:Sine.easeInOut});
+    }*/
+
+	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+	animatedDiv.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"2%",
+		top:"2%",
+		backgroundColor:randomColor
+		});
+		
+	//animatedDiv.load(onloadHandler);
+	onloadHandler();
+
+	var next = $('<div id="'+targetDiv+'a"></div>');
+	animatedDiv.append(next);
+
+	ARM.recursivetest(targetDiv+'a',duration,sizeH*0.95,sizeV*0.95,number-1);
+	 }
+}
+
+
+
+ARM.tweenTonelVertical = function(targetDiv,duration,sizeH,sizeV,aditionalArgs){
+	aditionalArgs = aditionalArgs || "";
+	var animatedDiv = $('#'+targetDiv);
+	TweenLite.set(animatedDiv, {transformPerspective:800});
+	animatedDiv.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",
+		left:"100px",
+		top:"100px"
+		});
+	return(TweenMax.fromTo(animatedDiv, duration, {rotationY:90, transformOrigin:"50% 50% 0"},{rotationY:0, transformOrigin:"50% 50% -1000",ease:Sine.easeInOut}));
+}
+
+
+ARM.tweenHangingImage = function(imagePath,targetImg,duration,sizeH,sizeV,aditionalArgs){
+
+	aditionalArgs = aditionalArgs || "";
+	var image = $('#'+targetImg);
+
+	//TweenLite.set(image, {transformPerspective:800});
+
+	image.css({position:"relative",
+		width:sizeH+"px",
+		height:sizeV+"px",	
+		});
+	//image.load(onloadHandler);
+	image.attr('src', imagePath);
+	return(TweenMax.from(image,duration, {rotationX:-90,autoAlpha:0, transformOrigin:"50% top",yoyo:true,repeat: 1,ease:Elastic.easeOut }));
+
+}
---
+++ 14_11_11_PresentacionProductoTonel/functionality.js
@@ -0,0 +1,60 @@
+
+
+
+$(document).ready(function() {
+
+/*	
+	ARM.clipedImageColumToSide("img/landscape_training.jpg",13,640,400,100,"toRight");
+	ARM.clipedImageColumToSide("img/landscape_training.jpg",14,640,400,100,"toLeft");
+ 	ARM.clipedImageColumToSide("img/landscape_training.jpg",15,640,400,200,"toRight");
+*/
+	
+/*
+	ARM.tonelVerticalElastico("side3d",5,400,300);
+	ARM.hangingImage("img/landscape_training.jpg","test",5,200,200);
+*/
+
+/*
+	ARM.tonelVertical("side3d",5,400,300);
+	ARM.hangingImage("img/landscape_training.jpg","test",5,200,200);
+*/
+
+
+//	ARM.recursivetest("side3d",5,400,300,5);
+/*var p = TweenMax.fromTo($("#test"), 0.7, { boxShadow: "0px 0px 0px 0px rgba(0,255,0,0.3)"}, 
+	{ boxShadow: "0px 0px 20px 10px rgba(0,255,0,0.7)",    repeat: -1,    yoyo: true,    ease: Linear.easeNone});
+*/
+
+
+
+/*ARM.timeline(ARM.tonelVerticalElastico("side3d",5,400,300),
+	ARM.hangingImage("img/landscape_training.jpg","test",5,200,200,",'-=2'")
+	);
+*/
+
+	/* var tl = new TimelineLite({paused:true})
+	 .add(ARM.tweenHangingImage("img/batlogo.png","productImg",5,600,850))
+	 .add(ARM.tweenTonelVertical("side3d",5,400,300));
+	 */
+
+	
+    var tonelSides = $(".tonelSide");
+ 	
+  
+	 var tl = new TimelineMax({paused:true	})
+	 .staggerFrom(tonelSides, 24, {scale:1,repeat:1,yoyo:true,  rotationX:360,  transformOrigin:"50% 0% -4400px", ease:Sine.easeInOut}, 4)
+	.staggerFrom(tonelSides, 24, {scale:1,repeat:1,  rotationY:360,  transformOrigin:"50% 0% -4400px", ease:Sine.easeInOut}, 4,"-=20")
+	// .staggerFrom(splitPrecio.chars, 0.8, {scale:0.25, autoAlpha:0,  rotationZ:-880,  transformOrigin:"200% 50% -300", ease:Sine.easeOut}, 0.15)
+	// .add(TweenMax.fromTo($("#productImg"), 1, { rotationY:5}, 	{rotationY:-5,    repeat: -1,    yoyo: true,    ease: Bounce.easeIn}))
+	 ;
+	 tl.timeScale(4);
+	 //tl.progress (1);
+	 tl.resume();
+	
+
+});
+
+
+
+
+
